22c2ecaf7bb8f03a599143506d2acc132db91375
Bhavesh Bhati <bhavesh.bhati@gmail.com>
2024-12-21 16:23:40 +0000

    Fix import

--- tests/test_gitAliasTest.py
+++ tests/test_gitAliasTest.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+import platform
 import subprocess
 from .utilities.helper import DevopsScenarioTest
 
